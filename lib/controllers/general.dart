1537f713968fc97f8b4c770e5a83ca1283b9eb0f
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-24 00:30:52 +0100

    error handler widget

---
+++ lib/controllers/general.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+Widget showSomethingWentWrong(
+    double hight, double width, String errorMessage1, String errorMessage2) {
+  var widget = Container(
+    margin: const EdgeInsets.all(0),
+    width: width * 0.9,
+    height: hight * 0.5,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          errorMessage1,
+          style: const TextStyle(fontSize: 15, color: Colors.red),
+        ),
+        const Icon(Icons.error, color: Colors.red, size: 100),
+        Text(
+          errorMessage2,
+          style: const TextStyle(fontSize: 18, color: Colors.red),
+        ),
+      ],
+    ),
+  );
+  return widget;
+}
