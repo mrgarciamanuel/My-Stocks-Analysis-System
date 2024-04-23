ccae79725f0d5020fa0b64bcc35ed884d1281ee9
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-23 22:38:30 +0100

    method to get companies

--- lib/controllers/companyController.dart
+++ lib/controllers/companyController.dart
@@ -1,6 +1,16 @@
 import 'package:my_stock_analsys/models/company.dart';
-//import 'package:httppackage/http.dart' as http;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<List<Company>> getCompanies() async {
-  //final response = await
+  String url = "https://api/teste";
+
+  final response = await http.get(Uri.parse(url));
+
+  if (response.statusCode == 200) {
+    List jsonResponse = json.decode(response.body)["data"];
+    return jsonResponse.map((company) => Company.fromJson(company)).toList();
+  } else {
+    throw Exception('Failed to load companies');
+  }
 }
