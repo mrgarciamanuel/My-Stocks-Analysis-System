1a0e861701a2ad17d9c05d735460102dd0b4f27f
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-23 22:31:03 +0100

    company controler created

---
+++ lib/controllers/companyController.dart
@@ -0,0 +1,6 @@
+import 'package:my_stock_analsys/models/company.dart';
+//import 'package:httppackage/http.dart' as http;
+
+Future<List<Company>> getCompanies() async {
+  //final response = await
+}
