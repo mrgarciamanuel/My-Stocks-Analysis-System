ef256e9efb20a33ead174e206f4603de14b4260b
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-04-26 10:03:18 +0100

    company model update

--- lib/models/company.dart
+++ lib/models/company.dart
@@ -1,26 +1,26 @@
 class Company {
   final String name;
+  final String simbol;
   final String location;
   final String imgURL;
   final String country;
   final String industry;
-  final String description;
 
   const Company({
     required this.name,
+    required this.simbol,
     required this.location,
     required this.imgURL,
     required this.country,
     required this.industry,
-    required this.description,
   });
 
   factory Company.fromJson(Map<String, dynamic> jsonObj) => Company(
         name: jsonObj['name'],
+        simbol: jsonObj['simbol'],
         location: jsonObj['location'],
         imgURL: jsonObj['imgURL'],
         country: jsonObj['country'],
         industry: jsonObj['industry'],
-        description: jsonObj['description'],
       );
 }
