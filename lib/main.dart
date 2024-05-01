ad94a9a33c7d4e96a3fdfa616942643b919c56ad
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-05-01 02:51:26 +0100

    btn to generate graph

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,38 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    Offset offSet = Offset(width - 120, height - 80);
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primaryColor: Colors.green,
-        ),
-        home: const HomePage());
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.green,
+      ),
+      home: const HomePage(),
+      builder: (context, child) {
+        return Stack(
+          children: [
+            child!,
+            Positioned(
+              left: offSet.dx,
+              top: offSet.dy,
+              child: GestureDetector(
+                /*onPanUpdate: (details) => StepState(() =>
+                    _offSet += Offset(details.delta.dx, details.delta.dy)),*/
+                child: FloatingActionButton(
+                  onPressed: () {
+                    debugPrint("Menu");
+                  },
+                  child: const Icon(Icons.auto_graph),
+                  backgroundColor: Colors.green,
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
   }
 }
