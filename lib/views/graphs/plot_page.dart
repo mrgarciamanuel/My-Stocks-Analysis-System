d212e67e99ffda63c13011dfcca5edd6e177d64b
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-05-01 11:18:44 +0100

    plot page init

---
+++ lib/views/graphs/plot_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:my_stock_analsys/controllers/general.dart';
+
+class PlotPage extends StatefulWidget {
+  const PlotPage({super.key});
+
+  @override
+  State<PlotPage> createState() => _PlotPageState();
+}
+
+class _PlotPageState extends State<PlotPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: buildAppBar(),
+        body: Container(
+          child: const Text("Plot Page"),
+        ));
+  }
+}
