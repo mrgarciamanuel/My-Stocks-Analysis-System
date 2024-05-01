095220e82db6e790ba33fff97c09c3fadda36531
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-05-01 11:55:53 +0100

    dropdown with list of graphs that can be ploted

--- lib/views/graphs/plot_page.dart
+++ lib/views/graphs/plot_page.dart
@@ -9,12 +9,59 @@ class PlotPage extends StatefulWidget {
 }
 
 class _PlotPageState extends State<PlotPage> {
+  String defaultDropdownValue = "line";
+  var items = [
+    const DropdownMenuItem(
+      value: "line",
+      child: Text("Line Graph"),
+    ),
+    const DropdownMenuItem(
+      value: "histogram",
+      child: Text("Histogram Graph"),
+    ),
+    const DropdownMenuItem(
+      value: "area",
+      child: Text("Area Graph"),
+    ),
+    const DropdownMenuItem(
+      value: "stacked",
+      child: Text("Stacked Graph"),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: buildAppBar(context, "Plot Page", true),
-        body: Container(
-          child: const Text("Plot Page"),
+        body: SizedBox(
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  DropdownButton(
+                      items: items,
+                      value: defaultDropdownValue,
+                      onChanged: (String? value) {
+                        setState(() {
+                          defaultDropdownValue = value!;
+                        });
+                      }),
+                  ElevatedButton(
+                      onPressed: () {
+                        showSnackBar(context, "You shoosed", 0);
+                      },
+                      child: const Text("Generate Graph"))
+                ],
+              ),
+              //this can be replaced wiht widget that will display the graph
+              Container(
+                margin: const EdgeInsets.all(10),
+                child: const Text("Graph will be displayed here",
+                    style: TextStyle(fontSize: 20)),
+              )
+            ],
+          ),
         ));
   }
 }
