508ccdb26adeabd6d9a496825db2d5f1ea844b32
Garcia Manuel <garciaisaiasmauel@gmail.com>
2024-05-04 15:18:43 +0100

    setting labels and values

--- lib/views/graphs/line_plot.dart
+++ lib/views/graphs/line_plot.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
 class LinePlot extends CustomPainter {
+  int nElements = 7;
+  List<List<Offset>> xPoints = [];
+  List<List<Offset>> yPoints = [];
+  List<String> labels = [
+    '01/05',
+    '02/05',
+    '03/05',
+    '04/05',
+    '05/05',
+    '06/05',
+    '07/05'
+  ];
+  List<int> yValues = [1, 2, 3, 4, 5, 6, 7];
   final customPaint = Paint()
     ..color = const Color.fromARGB(255, 0, 0, 0)
     ..strokeWidth = 2
@@ -11,9 +24,9 @@ class LinePlot extends CustomPainter {
   ///size: is the size of the area where we are going to draw
   ///the function return an int value in where will start building the markers of this axis
   List<int> drawYAxis(Canvas canvas, Size size) {
-    double x1 = 20;
-    double x2 = 20;
-    double y1 = 20;
+    double x1 = 30;
+    double x2 = 30;
+    double y1 = 10;
     double y2 = size.height - 10;
 
     final p1 = Offset(x1, y1);
@@ -30,13 +43,13 @@ class LinePlot extends CustomPainter {
   ///designs the x axis
   ///canvas: is the object we are going to draw
   ///size: is the size of the area where we are going to draw
-  ///the function return an int value in where will start building the markers of this axis
+  ///returns an array containing: firs two are coordinates where we'll start building the markers of this axis and the last one are real points in the x axis
   List<int> drawXAxis(Canvas canvas, Size size) {
     //par de valores para x e y
     const double x1 = 10;
-    double x2 = size.width - 20;
-    double y1 = size.height - 20;
-    double y2 = size.height - 20;
+    double x2 = size.width - 10;
+    double y1 = size.height - 30;
+    double y2 = size.height - 30;
 
     final p1 = Offset(x1, y1);
     final p2 = Offset(x2, y2);
@@ -46,31 +59,76 @@ class LinePlot extends CustomPainter {
       ..style = PaintingStyle.stroke
       ..strokeWidth = 1;
     canvas.drawLine(p1, p2, paint);
-    return [x1.toInt() + 10, y1.toInt()];
+    return [x1.toInt() + 10, y1.toInt(), x1.toInt()];
   }
 
-  void drawXMarkers(Canvas canvas, Size size, double startX, double startY) {
+  ///desenha marcadores no eixo X
+  void drawXMarkers(Canvas canvas, Size size, double startX) {
     double separator = 45;
     double x = startX;
 
-    for (int i = 0; i < 7; i++) {
-      final p1 = Offset(x + separator, size.height - 10);
-      final p2 = Offset(x + separator, size.height - 30);
+    for (int i = 0; i < nElements; i++) {
+      final p1 = Offset(x + separator, size.height - 25);
+      final p2 = Offset(x + separator, size.height - 35);
+      final p3 = Offset(x + separator, size.height - 30);
       canvas.drawLine(p1, p2, customPaint);
+      xPoints.add([p1, p2, p3]);
       separator += 45;
     }
   }
 
-  void drawYMarkers(Canvas canvas, Size size, double startX, double startY) {
+  ///desenha marcadores no eixo Y
+  void drawYMarkers(Canvas canvas, Size size, double startX) {
     double separator = 45;
     double x = startX;
     double y = size.height - 20;
-    for (int i = 0; i < 7; i++) {
-      final p1 = Offset(x, y - separator);
-      final p2 = Offset(x + 20, y - separator);
+    for (int i = 0; i < nElements; i++) {
+      final p1 = Offset(x + 5, y - separator);
+      final p2 = Offset(x + 15, y - separator);
+      final p3 = Offset(x + 10, y - separator);
       canvas.drawLine(p1, p2, customPaint);
+      yPoints.add([p1, p2, p3]);
       separator += 45;
     }
+  }
+
+  ///vai desenhar o ponto de encontro entre os eixos x e y
+  void drawPoint(Canvas canvas, Offset point) {
+    final paint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 3
+      ..style = PaintingStyle.fill;
+    canvas.drawCircle(point, 3, paint);
+  }
+
+  setText(String text, Canvas canvas, size, Offset location, String axis) {
+    const textStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 12,
+    );
+
+    var textSpan = TextSpan(
+      text: text,
+      style: textStyle,
+    );
+
+    var textPainter = TextPainter(
+      text: textSpan,
+      textDirection: TextDirection.ltr,
+    );
+
+    textPainter.layout(
+      minWidth: 0,
+      maxWidth: size.width,
+    );
+
+    debugPrint(location.toString());
+
+    textPainter.paint(
+      canvas,
+      Offset(axis == "x" ? location.dx - 10 : location.dx - 10,
+          axis == "x" ? location.dy : location.dy - 10),
+    );
   }
 
   ///size: é o tamanho da area onde vamos desenhar
@@ -90,12 +148,17 @@ class LinePlot extends CustomPainter {
     drawXAxis(canvas, size);
     drawYAxis(canvas, size);
     int startXaxiX = drawXAxis(canvas, size)[0];
-    int startYaxiX = drawYAxis(canvas, size)[1];
-
     int startXaxiY = drawYAxis(canvas, size)[0];
-    int startYaxiY = drawYAxis(canvas, size)[1];
-    drawXMarkers(canvas, size, startXaxiX.toDouble(), startYaxiX.toDouble());
-    drawYMarkers(canvas, size, startXaxiY.toDouble(), startYaxiY.toDouble());
+
+    drawXMarkers(canvas, size, startXaxiX.toDouble());
+    drawYMarkers(canvas, size, startXaxiY.toDouble());
+
+    for (int i = 0; i < xPoints.length; i++) {
+      drawPoint(canvas, xPoints[i][2]);
+      drawPoint(canvas, yPoints[i][2]);
+      setText(yValues[i].toString(), canvas, size, yPoints[i][0], "y");
+      setText(labels[i], canvas, size, xPoints[i][0], "x");
+    }
   }
 
   @override
